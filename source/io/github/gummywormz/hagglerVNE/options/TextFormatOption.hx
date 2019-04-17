168992d8c3f0b563db98ee42faeeafa2c37da45a
Paul Alves <pja1996+github@gmail.com>
2019-04-16 20:52:01 -0500

    Update TextFormatOption.hx
    fix compilation issues

--- source/io/github/gummywormz/hagglerVNE/options/TextFormatOption.hx
+++ source/io/github/gummywormz/hagglerVNE/options/TextFormatOption.hx
@@ -10,13 +10,13 @@ import flixel.util.FlxColor;
 class TextFormatOption
 {
 	//setFormat(?Font:String, Size:Int = 8, Color:FlxColor = FlxColor.WHITE, ?Alignment:FlxTextAlign, ?BorderStyle:FlxTextBorderStyle, BorderColor:FlxColor = FlxColor.TRANSPARENT, EmbeddedFont:Bool = true)
-	var font(get, set) : String;
-	var size(get, set) : Int;
-	var textColor(get, set) : FlxColor;
-	var align(get, set) : FlxTextAlign;
-	var borderStyle(get, set) : FlxTextBorderStyle;
-	var borderColor(get, set) :FlxColor;
-	var embedded(get, set) : Bool;
+	@:isVar var font(get, set) : String;
+	@:isVar var size(get, set) : Int;
+	@:isVar var textColor(get, set) : FlxColor;
+	@:isVar var align(get, set) : FlxTextAlign;
+	@:isVar var borderStyle(get, set) : FlxTextBorderStyle;
+	@:isVar var borderColor(get, set) :FlxColor;
+	@:isVar var embedded(get, set) : Bool;
 
 	public function new(?Font:String, Size:Int = 8, Color:FlxColor = FlxColor.WHITE, ?Alignment:FlxTextAlign, ?BorderStyle:FlxTextBorderStyle, BorderColor:FlxColor = FlxColor.TRANSPARENT, EmbeddedFont:Bool = true)
 	{
@@ -29,79 +29,74 @@ class TextFormatOption
 		embedded = EmbeddedFont;
 	}
 	
-	function get_embedded():Bool 
+	public function get_embedded():Bool 
 	{
 		return embedded;
 	}
 	
-	function set_embedded(value:Bool):Bool 
+	public function set_embedded(value:Bool):Bool 
 	{
 		return embedded = value;
 	}
 	
-	function get_borderColor(): 
-	{
-		return borderColor;
-	}
-	
 
 	
-	function get_borderStyle():FlxTextBorderStyle 
+	public function get_borderStyle():FlxTextBorderStyle 
 	{
 		return borderStyle;
 	}
 	
-	function set_borderStyle(value:FlxTextBorderStyle):FlxTextBorderStyle 
+	public function set_borderStyle(value:FlxTextBorderStyle):FlxTextBorderStyle 
 	{
 		return borderStyle = value;
 	}
 	
-	function get_align():FlxTextAlign 
+	public function get_align():FlxTextAlign 
 	{
 		return align;
 	}
 	
-	function set_align(value:FlxTextAlign):FlxTextAlign 
+	public function set_align(value:FlxTextAlign):FlxTextAlign 
 	{
 		return align = value;
 	}
 	
-	function get_borderColor():FlxColor 
+	public function get_borderColor():FlxColor 
 	{
 		return borderColor;
 	}
 	
-	function set_borderColor(value:FlxColor):FlxColor 
+	public function set_borderColor(value:FlxColor):FlxColor 
 	{
 		return borderColor = value;
 	}
 	
-	function get_textColor():FlxColor 
+	public function get_textColor():FlxColor 
 	{
 		return textColor;
 	}
 	
-	function set_textColor(value:FlxColor):FlxColor 
+	public function set_textColor(value:FlxColor):FlxColor 
 	{
 		return textColor = value;
 	}
 	
-	function get_size():Int 
+	public function get_size():Int 
 	{
 		return size;
 	}
 	
-	function set_size(value:Int):Int 
+	public function set_size(value:Int):Int 
 	{
 		return size = value;
 	}
 	
-	function get_font():String 
+	public function get_font():String 
 	{
 		return font;
 	}
 	
-	function set_font(value:String):String 
+	public function set_font(value:String):String 
 	{
 		return font = value;
 	}
