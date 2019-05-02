fee8e19028273a582488cb360b2981beab02b9e2
Paul Alves <pja1996+github@gmail.com>
2019-05-01 20:34:01 -0500

    5/1/2019 3
    update documentation on global options

--- source/io/github/gummywormz/hagglerVNE/options/GlobalOptions.hx
+++ source/io/github/gummywormz/hagglerVNE/options/GlobalOptions.hx
@@ -1,8 +1,18 @@
 package io.github.gummywormz.hagglerVNE.options ;
 
+/**
+   This class holds global options used by default by the game engine
+   This is used primarily to cover very common options like the default text color.
+**/
 class GlobalOptions
 {
+	/**
+	   The default TextFormatOption. This gets applied to all dialogues by default unless one is manually added to the dialogue.
+	**/
 	public static var textFormat : TextFormatOption;
+	/**
+	   Global dialogue option. Referenced by Dialogue.fromGDialogueOption. Does not apply on its own.
+	**/
 	public static var dialogueOption : DialogueOption;
 	
 }
